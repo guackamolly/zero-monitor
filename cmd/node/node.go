308b89f46597a89a6c031c8b859a67d7e2cfeb8e
João Freitas <1160907@isep.ipp.pt>
2024-10-15 20:23:46 +0100

    feat: initialize pub server

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/guackamolly/zero-monitor/internal/conn"
+	"github.com/guackamolly/zero-monitor/internal/data/repositories"
+	"github.com/guackamolly/zero-monitor/internal/mq"
+	"github.com/guackamolly/zero-monitor/internal/service"
 )
 
 func main() {
+	// 1. Initialize DI.
+	pc := createPublishContainer()
+	ctx := context.Background()
+	ctx = mq.InjectPublishContainer(ctx, pc)
+
+	// 2. Find master node in local network.
 	conn, err := conn.StartBeaconBroadcast()
 	if err != nil {
 		log.Fatalf("failed to probe master node, %v\n", err)
 	}
 
-	log.Printf("found master node, %v\n", conn)
+	// 3. Initialize pub server.
+	s := mq.NewPubSocket(ctx)
+	defer s.Close()
+
+	err = mq.ConnectPublish(s, conn.IP, conn.Port)
+	if err != nil {
+		log.Fatalf("coudln't open zeromq pub socket, %v\n", err)
+	}
+	log.Printf("started zeromq pub socket on addr: %s\n", s.Addr())
+	s.RegisterPublishers()
+
+	// 4. Await termination...
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+}
+
+func createPublishContainer() mq.PublishContainer {
+	system := repositories.GopsUtilSystemRepository{}
+
+	return mq.PublishContainer{
+		NodeReporter: service.NewNodeReporterService(system),
+	}
 }
