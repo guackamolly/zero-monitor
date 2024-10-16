a0234138b0986a72913aa2caccd786bd0f9faad4
João Freitas <1160907@isep.ipp.pt>
2024-10-16 10:40:59 +0100

    refactor: use map for templates

--- internal/http/template.go
+++ internal/http/template.go
@@ -1,34 +1,40 @@
 package http
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 
+	"github.com/guackamolly/zero-monitor/internal/logging"
 	"github.com/labstack/echo/v4"
 )
 
 type Template struct {
-	templates *template.Template
+	templates map[string]*template.Template
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	err := t.templates.ExecuteTemplate(w, templates[name], data)
-
-	if err != nil {
-		c.Logger().Error(err)
+	tpl, ok := t.templates[name]
+	if !ok {
+		return fmt.Errorf("failed to find template for name: %s", name)
 	}
 
-	return err
+	return tpl.Execute(w, data)
 }
 
 func RegisterTemplates(e *echo.Echo) error {
-	tps := make([]string, 0, len(templates))
-	for _, v := range templates {
-		tps = append(tps, v)
+	tps := map[string]*template.Template{}
+	for k, v := range templates {
+		t, err := template.New(k).ParseFiles(v)
+		if err != nil {
+			logging.LogFatal("failed to parse template file, %v", err)
+		}
+
+		tps[k] = t
 	}
 
 	t := &Template{
-		templates: template.Must(template.ParseFiles(tps...)),
+		templates: tps,
 	}
 
 	e.Renderer = t
